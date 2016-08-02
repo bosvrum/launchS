100dbc43958ec05e16cd6d3bdae130f8b1f5a980
Catalin Boruga <catalinboruga@yahoo.com>
2016-08-02 17:06:54 +0200

    First Question

---
+++ lesson_3/Easy1/1-question.rb
@@ -0,0 +1,6 @@
+#What would you expect the code below to print out?
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+# it will return an array with all the values [1,2,2,3], we are not mutating the array
